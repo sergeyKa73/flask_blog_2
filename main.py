f93fea67f13d152e15748a9faa394e30c5eabcc0
Sergey <karandeev73@gmail.com>
2022-01-16 12:09:27 +0300

    feat: add posts page and display data from the DB

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,34 +16,49 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"<Article {self.id}>"
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/create_artirle', methods=['POST', 'GET'])
-def create_artirle():
-    if request.method =='POST':
+
+@app.route(('/posts'))
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route(('/posts/<int:id>'))
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+
+@app.route('/create_article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
 
-        article =Article(title=title, intro=intro, text=text)
+        article = Article(title=title, intro=intro, text=text)
 
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return 'При добавлении статьи произошла ошибка'
     else:
-        return render_template('create_artirle.html')
+        return render_template('create_article.html')
+
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
