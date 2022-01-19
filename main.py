c7cb253456ec9ffa70376092b7b96235a1cfdc3e
Sergey <karandeev73@gmail.com>
2022-01-19 19:43:31 +0300

    fix: to fix bugs

--- main.py
+++ main.py
@@ -24,10 +24,50 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html')\
 
-@app.route('/create_artirle', methods=['POST', 'GET'])
-def create_artirle():
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+@app.route('/posts/<int:id>/del')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'При удалении статьи произошла ошибка'
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method =='POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При редактировании статьи произошла ошибка'
+    else:
+        return render_template('post_update.html', article=article)
+
+
+@app.route('/create_article', methods=['POST', 'GET'])
+def create_article():
     if request.method =='POST':
         title = request.form['title']
         intro = request.form['intro']
@@ -38,11 +78,11 @@ def create_artirle():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return 'При добавлении статьи произошла ошибка'
     else:
-        return render_template('create_artirle.html')
+        return render_template('create_article.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
